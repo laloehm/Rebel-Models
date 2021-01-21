b1325d8bdfae1a09579f3ed5483953fefc1c06eb
laloehm <laloehm1@gmail.com>
2021-01-20 21:37:25 -0600

    Version 1.0

--- js/app.js
+++ js/app.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+/* Smooth scrolling para anclas */
+$("a.smooth").on("click", function (e) {
+    e.preventDefault();
+    var $link = $(this);
+    var anchor = $link.attr("href");
+    $("html, body").stop().animate({
+		scrollTop: $(anchor).offset().top - 100 + "px"
+	}, 1000);
+	$('#nav-icon').toggleClass('open');
+});
+
+new WOW().init();
